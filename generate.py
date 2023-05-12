f3bf0ae911caef0192a732ffe1a996c86607dd58
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2023-05-13 00:00:22 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,13 +6,13 @@ def generate_random_redirects(num_redirects):
     for _ in range(num_redirects):
         source = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
         target = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
-        redirect = f'RewriteRule ^/{source}$ https://dev.interbau-ag.de/{target} [R=301,L]'
+        redirect = f'RewriteRule ^/{source}$ https://example.de/{target} [R=301,L]'
         redirects.append(redirect)
     return redirects
 
 def generate_mywebsite_conf(num_redirects):
     redirects = generate_random_redirects(num_redirects)
-    redirects.append('RewriteRule ^(.*)$ https://dev.interbau-ag.de/$1 [R=301,L]')
+    redirects.append('RewriteRule ^(.*)$ https://example.de/$1 [R=301,L]')
     conf_content = '\n'.join(redirects)
 
     with open('mywebsite.conf', 'w') as file:
