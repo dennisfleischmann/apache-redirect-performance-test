ec252b321369cf7f08632bc7b26b1559d3dfd906
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2023-05-13 20:17:14 +0530

    Adjusted script

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-docker build --build-arg NUM_REDIRECTS=100 -t my-apache-image-100 .
-docker build --build-arg NUM_REDIRECTS=300000 -t my-apache-image-300000 .
-docker build --build-arg NUM_REDIRECTS=550000 -t my-apache-image-550000 .
+sudo docker build --build-arg NUM_REDIRECTS=100 -t my-apache-image-100 .
+sudo docker build --build-arg NUM_REDIRECTS=300000 -t my-apache-image-300000 .
+sudo docker build --build-arg NUM_REDIRECTS=550000 -t my-apache-image-550000 .
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-docker run -d -p 80:80 apache-redirect-100
-docker run -d -p 81:80 apache-redirect-300000
-docker run -d -p 82:80 apache-redirect-550000
+sudo docker run -d -p 80:80 apache-redirect-100
+sudo docker run -d -p 81:80 apache-redirect-300000
+sudo docker run -d -p 82:80 apache-redirect-550000
